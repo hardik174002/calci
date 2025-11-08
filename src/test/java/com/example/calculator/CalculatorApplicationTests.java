bd619d0ab6a66a89d797e2af04d4b0d0a0c91788
hardik174002 <hvhardik0@gmail.com>
2025-11-08 10:08:00 -0800

    Hey added doms test check it

--- src/test/java/com/example/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/example/calculator/CalculatorApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.calculator;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,12 @@ class CalculatorApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(2 + 2 == 4);
+	}
+
+	@Test
+	void sampleTest() {	
+		assertTrue(1 + 1 == 3);
 	}
 
 }
